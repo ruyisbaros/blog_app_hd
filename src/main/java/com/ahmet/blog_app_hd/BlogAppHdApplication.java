316b540ff32c82c5ad71b8eb402a88f1d4232595
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-09-26 17:04:30 +0200

    first commit

---
+++ src/main/java/com/ahmet/blog_app_hd/BlogAppHdApplication.java
@@ -0,0 +1,13 @@
+package com.ahmet.blog_app_hd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppHdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppHdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmet/blog_app_hd/BlogAppHdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmet.blog_app_hd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppHdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
